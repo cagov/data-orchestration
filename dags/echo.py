358a4cdc8404d3c32792226b055395493471ef30
Ian Rose <ian.rose@innovation.ca.gov>
2023-01-23 12:53:33 -0800

    Demonstrate use of container resources.

--- dags/echo.py
+++ dags/echo.py
@@ -6,6 +6,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 from common.defaults import DEFAULT_ARGS
+from kubernetes.client import models as k8s_models
 
 # Define a DAG (directed acyclic graph) of tasks.
 # Any task you create within the context manager is automatically added to the
@@ -19,9 +20,12 @@ with models.DAG(
     kubernetes_min_pod = KubernetesPodOperator(
         task_id="echo",
         name="echo",
-        arguments=["micromamba", "env", "list"],
+        arguments=["cat", "/proc/cpuinfo"],
         namespace="composer-user-workloads",
         image="us-west1-docker.pkg.dev/caldata-sandbox/dse-orchestration-us-west1/geo:latest",
         kubernetes_conn_id="kubernetes_default",
         config_file="/home/airflow/composer_kube_config",
+        container_resources=k8s_models.V1ResourceRequirements(
+            requests={"memory": "32Gi", "cpu": "8"},
+        ),
     )
